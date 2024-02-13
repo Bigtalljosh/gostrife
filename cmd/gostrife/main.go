1ded877fc2b30fb75a381478d9304adaa60d1939
Josh Dadak <Bigtalljosh@users.noreply.github.com>
2024-02-13 10:54:35 +0000

    added logger, config management

--- cmd/gostrife/main.go
+++ cmd/gostrife/main.go
@@ -2,21 +2,24 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
 
 	"github.com/Bigtalljosh/gostrife/internal/services"
-	// TODO: Ask Antony the correct way to do modules, because this can't be it
-	customers "github.com/Bigtalljosh/gostrife/internal/web/customers"
-	landing "github.com/Bigtalljosh/gostrife/internal/web/landing"
+	"github.com/Bigtalljosh/gostrife/internal/web/customers"
+	"github.com/Bigtalljosh/gostrife/internal/web/landing"
 	"github.com/gorilla/mux"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
 )
 
 func main() {
+		
+	SetupLogging()
+	LoadConfig()
 
 	r := mux.NewRouter()
 
@@ -29,15 +32,16 @@ func main() {
 	landingController.RegisterRoutes(r)
 
 	server := &http.Server{
-		Addr: ":8080",
+		Addr: viper.GetString("server.port"),
 		Handler: r,
 	}
 
 	go func() {
-        fmt.Println("Server running on port 8080")
-
+		log.Info().
+		Str("Server running on port", viper.GetString("server.port"))
+        
         if err := server.ListenAndServe(); err != nil {
-            log.Fatal(err)
+            log.Fatal().Err(err)
         }
     }()
 
@@ -46,13 +50,37 @@ func main() {
     c := make(chan os.Signal, 1)
     signal.Notify(c, os.Interrupt)
     <-c
-    fmt.Println("\nShutting down server...")
+    log.Info().Msg("Shutting down server...")	
 
     ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
     defer cancel()
     if err := server.Shutdown(ctx); err != nil {
-        log.Fatal(err)
+        log.Error().Err(err)
     }
 
-    fmt.Println("Server gracefully stopped")
+    log.Info().Msg("Server gracefully stopped")
+}
+
+func SetupLogging() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	// Add file and line number to log
+	log.Logger = log.With().Caller().Logger()
+
+    log.Print("logger set up")
+}
+
+func LoadConfig(){
+	viper.SetConfigFile("./config.json")
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; ignore error if desired
+			log.Fatal().Err(err).Msg("Config file not found")
+		} else {
+			// Config file was found but another error was produced
+			log.Fatal().Err(err).Msg("Config found, error parsing")
+		}
+	}
+	// Config file found and successfully parsed
 }
--- internal/web/landing/landing_controller.go
+++ internal/web/landing/landing_controller.go
@@ -1,4 +1,4 @@
-package web
+package landing
 
 import (
 	"net/http"
